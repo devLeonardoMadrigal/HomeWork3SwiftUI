266631d1a6cb2e8b4cc2b2ce522f94c6ffa02d81
Leonardo Madrigal <DevLeonardoDavid@gmail.com>
2026-03-19 16:06:20 -0400

    Initial Commit

---
+++ HomeWork3SwiftUI/HomeWork3SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3SwiftUIApp.swift
+//  HomeWork3SwiftUI
+//
+//  Created by Leonardo Madrigal on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
